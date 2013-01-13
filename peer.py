8108b858186a07a21bb5c8bc704e9de1f5be3110
Dionysis Zindros <dionyziz@gmail.com>
2013-01-13 14:01:20 +0200

    Networking code

--- peer.py
+++ peer.py
@@ -26,14 +26,22 @@ class Peer:
 
     __socket = None
 
-    __lastMsg = ''
+    __lastMsgSent = ''
+    __lastMsgRecv = ''
     __buffer = ''
 
+    def __log( self, msg ):
+        log.info( '[peer %s:%i] %s' % ( self.ip, self.port. msg ) )
     def __beginSend( self, msg ):
-        self.__lastMsg = msg
-        log.info( 'Sending %s to peer %s:%i.' % ( msg, self.ip, self.port ) )
+        self.__lastMsgSent = msg
+        log.info( 'Sending %s.' % msg )
     def __finishSend( self ):
-        log.info( '%s sent to peer %s:%i.' % ( self.__lastMsg, self.ip, self.port ) )
+        log.info( 'Sent %s.' % self.__lastMsg )
+    def __beginRecv( self, msg ):
+        self.__lastMsgRecv = msg
+        log.info( 'Receiving %s.' % msg )
+    def __finishRecv( self ):
+        log.info( 'Received %s.' % self.__lastMsg )
     def __sendByte( self, b ):
         self.socket.send( struct.pack( '!B', b ) )
     def __sendInt( self, i ):
@@ -124,27 +132,63 @@ class Peer:
         self.__sendTorrentMessage( 8, struct.pack( '!III', index, begin, length ) )
         self.__finishSend()
         pass
-    def __handleHandshake( self ):
+    def __handleHandshake( self, infoHash, peerId ):
+        self.__beginRecv( 'handshake' )
+        # TODO: handle handshake
+        self.__finishRecv()
         pass
     def __handleKeepAlive( self ):
+        self.__beginRecv( 'keep-alive' )
+        self.__finishRecv()
         pass
     def __handleChoke( self ):
+        self.__beginRecv( 'choke' )
+        # TODO: handle choke
+        self.__finishRecv()
         pass
     def __handleUnchoke( self ):
+        self.__beginRecv( 'unchoke' )
+        # TODO: handle unchoke
+        self.__finishRecv()
         pass
     def __handleInterested( self ):
+        self.__beginRecv( 'interested' )
+        # TODO: handle interested
+        self.__finishRecv()
         pass
     def __handleUninterested( self ):
+        self.__beginRecv( 'uninterested' )
+        # TODO: handle uninterested
+        self.__finishRecv()
         pass
-    def __handleHave( self ):
+    def __handleHave( self, pieceIndex ):
+        self.__beginRecv( 'have' )
+        # TODO: handle have
+        self.__finishRecv()
         pass
-    def __handleBitfield( self ):
+    def __handleBitfield( self, bitField ):
+        # TODO: assert the length of the bitfield matches the number of pieces
+        self.__beginRecv( 'bitfield' )
+        # TODO: handle bitfield
+        self.__finishRecv()
         pass
     def __handleRequest( self ):
+        self.__beginRecv( 'request' )
+        # TODO: handle request
+        self.__finishRecv()
         pass
-    def __handlePiece( self ):
+    def __handlePiece( self, index, begin, block ):
+        # TODO: assert index is less than the total number of pieces
+        # TODO: assert begin is less than piece size
+        # TODO: assert that the whole block is within the piece
+        self.__beginRecv( 'piece' )
+        # TODO: handle piece
+        self.__finishRecv()
         pass
     def __handleCancel( self ):
+        self.__beginRecv( 'cancel' )
+        # TODO: handle cancel
+        self.__finishRecv()
         pass
     def __handleIncomingBytes( self, bytes ):
         # some bytes have arrived
@@ -158,7 +202,7 @@ class Peer:
             # wait for more bytes
             return
         # get the length of the message
-        length = struct.unpack( '!I', self.__buffer[ :4 ] )
+        length = struct.unpack( '!I', self.__buffer[ :4 ] )[ 0 ]
         # length is the length of the following message
         # check if we have yet received all the data promised for the message
         if len( self.__buffer ) < 4 + length:
@@ -185,40 +229,40 @@ class Peer:
     def __handleTorrentMessage( self, messageId, payload, length = None ):
         if length == 0:
             # 0-length message is a keep-alive
-            self.__handleKeepAlive()
-            return
-        if messageId == 0:
+            return self.__handleKeepAlive()
+        if messageId == 0: # choke
             assert( length == 1 )
-            self.__handleChoke()
-            return
-        if messageId == 1:
+            return self.__handleChoke()
+        if messageId == 1: # unchoke
             assert( length == 1 )
-            self.__handleUnchoke()
-            return
-        if messageId == 2:
+            return self.__handleUnchoke()
+        if messageId == 2: # interested
             assert( length == 1 )
-            self.__handleInterested()
-            return
-        if messageId == 3:
+            return self.__handleInterested()
+        if messageId == 3: # uninterested
             assert( length == 1 )
-            self.__handleUninterested()
-            return
-        if messageId == 4:
-            # TODO: have
-            return
-        if messageId == 5:
-            # TODO: bitfield
-            return
-        if messageId == 6:
-            # TODO: request
-            return
-        if messageId == 7:
-            # TODO: piece
-            return
-        if messageId == 8:
-            # TODO: cancel
-            return
-        log.warning( 'Unknown p2p wire protocol message id %i.' % messageId ) 
+            return self.__handleUninterested()
+        if messageId == 4: # have
+            assert( length == 5 )
+            pieceIndex = unpack( '!I', payload )[ 0 ]
+            return self.__handleHave( payload )
+        if messageId == 5: # bitfield
+            return self.__handleBitfield( payload )
+        if messageId == 6: # request
+            assert( length == 13 )
+            ( requestIndex, requestBegin, requestLength ) = unpack( '!III', payload )
+            return self.__handleRequest( requestIndex, requestBegin, requestLength )
+        if messageId == 7: # piece
+            pieceLocation = payload[ :8 ]
+            ( pieceIndex, pieceBegin ) = unpack( '!II', pieceLocation )
+            pieceBlock = payload[ 8: ]
+            return self.__handlePiece( pieceIndex, pieceBegin, pieceBlock )
+        if messageId == 8: # cancel
+            assert( length == 13 )
+            ( cancelIndex, cancelBegin, cancelLength ) = unpack( '!III', payload )
+            return self.__handleCancel( cancelIndex, cancelBegin, cancelLength )
+        # TODO: handshake
+        self.__log( 'Unknown p2p wire protocol message id %i.' % messageId ) 
     def choke( self ):
         if self.__localChoke:
             return
@@ -242,15 +286,19 @@ class Peer:
         
     def connect( self ):
         self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-        log.info( 'Connecting to peer %s:%i' % ( self.ip, self.port ) + '...' )
+        self.__log( 'Connecting...' )
         self.socket.settimeout( 3 )
         try:
             self.socket.connect( ( self.ip, self.port ) )
         except:
-            log.info( 'Connection to peer %s:%i failed.' % ( self.ip, self.port ) )
+            self.__log( 'Connection failed.' )
             return
-        log.info( 'Connected to peer %s:%i successfully.' % ( self.ip, self.port ) )
+        self.__log( 'Connected.' )
         self.__sendHandshake()
+        self.__log( 'Waiting for data...' )
+        data = self.socket.recv( 1024 )
+        self.__log( 'Got some data of length %i' % len( data ) )
+        self.__handleIncomingBytes( data )
     def __generateId( self ):
         self.id = '-DZ0000-' + "".join( map( chr, [ random.randint( 0, 255 ) for i in range( 0, 12 ) ] ) )
     def __init__( self, torrent, ip = None, port = None ):
